989aa88348f10f93227c8fc8a6720c3f527debd1
Enoc Mena <enocschool@Enocs-iMac.local>
2021-07-13 13:18:29 -0400

    Initial commit

---
+++ src/test/java/com/ex28/AddingNumbersTest.java
@@ -0,0 +1,14 @@
+package com.ex28;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddingNumbersTest {
+
+    @org.junit.jupiter.api.Test
+    void addingNumbers() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
